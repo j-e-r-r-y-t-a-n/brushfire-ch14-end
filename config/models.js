7f8077d79ecf1690896ed3ca2551cb9399a4fc2f
Jerry Tan <jtan@dtcc.com>
2016-06-12 12:15:11 -0400

    Added mailgun credentials

--- config/models.js
+++ config/models.js
@@ -1,34 +1,42 @@
 /**
- * Default model configuration
- * (sails.config.models)
+ * Production environment settings
  *
- * Unless you override them, the following properties will be included
- * in each of your models.
+ * This file can include shared settings for a production environment,
+ * such as API keys or remote database passwords.  If you're using
+ * a version control solution for your Sails app, this file will
+ * be committed to your repository unless you add it to your .gitignore
+ * file.  If your repository will be publicly viewable, don't add
+ * any private information to this file!
  *
- * For more info on Sails models, see:
- * http://sailsjs.org/#!/documentation/concepts/ORM
  */
 
-module.exports.models = {
+module.exports = {
+
+  models: {
+    connection: 'productionPostgresqlServer'
+  },
 
   /***************************************************************************
-  *                                                                          *
-  * Your app's default connection. i.e. the name of one of your app's        *
-  * connections (see `config/connections.js`)                                *
-  *                                                                          *
-  ***************************************************************************/
-  connection: 'myPostgresqlServer',
+   * Set the default database connection for models in the production        *
+   * environment (see config/connections.js and config/models.js )           *
+   ***************************************************************************/
+
+  // models: {
+  //   connection: 'someMysqlServer'
+  // },
 
   /***************************************************************************
-  *                                                                          *
-  * How and whether Sails will attempt to automatically rebuild the          *
-  * tables/collections/etc. in your schema.                                  *
-  *                                                                          *
-  * See http://sailsjs.org/#!/documentation/concepts/ORM/model-settings.html  *
-  *                                                                          *
-  ***************************************************************************/
-  migrate: 'drop',
+   * Set the port in the production environment to 80                        *
+   ***************************************************************************/
 
-  schema: true
+  // port: 80,
+
+  /***************************************************************************
+   * Set the log level in production environment to "silent"                 *
+   ***************************************************************************/
+
+  // log: {
+  //   level: "silent"
+  // }
 
 };
